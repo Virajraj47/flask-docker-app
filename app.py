bbc2176648d8fc06085e5a9622002866a22a3a15
Viraj <virajrajput610@gmail.com>
2026-04-15 01:13:19 +0530

    added html UI

--- app.py
+++ app.py
@@ -4,7 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Deployment successful 🚀 VirajCloud Live!!"
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>VirajCloud</title>
+    </head>
+    <body>
+      <h1>Welcome to VirajCloud 🚀</h1>
+      <p>Deployment successful via CI/CD</p>
+    </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
